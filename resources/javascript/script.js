43e0e56b353bc4d2c6353c4cbe50e2248dbcc931
CHARLES MEARA <S4094575@student.rmit.edu.au>
2026-03-11 14:16:28 +1100

    Feat: implemented form validation

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -2,10 +2,21 @@ function signUp() {
     window.open("/html/signup.html");
 }
 
-function home() {
-    window.open("/html/home.html")
-}
-
 function validateSignUp(){
-    
+    alert("SUBMISSION");
+    let email = document.forms["sign-up-form"]["email"].value;
+    let pass1 = document.forms["sign-up-form"]["pass"].value;
+    let pass2 = document.forms["sign-up-form"]["pass2"].value;
+
+    if (pass1 != pass2){
+        alert("Passwords do not match!");
+        return false;
+    }
+
+    if (email.length() < 4){
+        alert("Email's fucked");
+        return false;
+    }
+
+    return true;
 }
